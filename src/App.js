e23b02b09c905ba21151cfdcf031572fbe354078
zer0p01nt <1027.bmy@gmail.com>
2025-01-26 03:47:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import ToDoList from "./routes/todos";
 import Converter from "./routes/converter";
@@ -6,14 +6,14 @@ import Coin from "./routes/coin";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Converter' element={<Converter />} />
         <Route path='/Coin-Tracker' element={<Coin />} />
         <Route path='/To-Do-List' element={<ToDoList />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
